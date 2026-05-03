ddba8878343a4e924e60ae0c89dd651768792b24
Pavel Salauyou <pavel.salauyou@gmail.com>
2026-05-03 19:05:06 +0200

    Add `Bar Chart` documentation: define routing structure, create API and common templates, and update overview content.

--- projects/docs/src/app/micro-charts/bar-chart/overview/overview.ts
+++ projects/docs/src/app/micro-charts/bar-chart/overview/overview.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { Page } from '@meta/page/page';
-import { PageContentDirective } from '@meta/page/page-content.directive';
 import { Playground } from '@meta/playground/playground';
 import {
   MchartBarBasicExample
@@ -20,13 +18,10 @@ import {
 import {
   MchartBarWithTooltipExample
 } from '../_examples/mchart-bar-with-tooltip-example/mchart-bar-with-tooltip-example';
-import { PageTitleDirective } from '@meta/page/page-title.directive';
 
 @Component({
-    selector: 'app-overview',
+  selector: 'app-overview',
   imports: [
-    Page,
-    PageContentDirective,
     Playground,
     MchartBarBasicExample,
     MchartBarCustomRadiusExample,
@@ -34,10 +29,9 @@ import { PageTitleDirective } from '@meta/page/page-title.directive';
     MchartBarResponsiveExample,
     MchartBarFillGradientExample,
     MchartBarWithTooltipExample,
-    PageTitleDirective
   ],
-    templateUrl: './overview.html',
-    styleUrl: './overview.scss'
+  templateUrl: './overview.html',
+  styleUrl: './overview.scss'
 })
 export class Overview {
 
