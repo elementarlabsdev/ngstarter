6e2f3b7b7422142b88d7c61578a132e614546359
Pavel Salauyou <pavel.salauyou@gmail.com>
2026-05-23 12:54:56 +0200

    Add new `Calendar` component with initial setup and tests

---
+++ projects/components/calendar/src/calendar/calendar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngs-calendar',
+  imports: [],
+  templateUrl: './calendar.html',
+  styleUrl: './calendar.scss',
+})
+export class Calendar {
+
+}
