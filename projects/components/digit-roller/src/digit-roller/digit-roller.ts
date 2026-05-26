9e36828e8611584a963f3b4b5a05830bbc092d23
Pavel Salauyou <pavel.salauyou@gmail.com>
2026-05-26 21:04:15 +0200

    Add new `DigitRoller` component with initial setup and tests: - Introduce base implementation, styles, and HTML template. - Add unit tests to ensure component creation. - Include `preview.svg` and `ng-package.json` for packaging and visualization.

---
+++ projects/components/digit-roller/src/digit-roller/digit-roller.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngs-digit-roller',
+  imports: [],
+  templateUrl: './digit-roller.html',
+  styleUrl: './digit-roller.scss',
+})
+export class DigitRoller {
+
+}
